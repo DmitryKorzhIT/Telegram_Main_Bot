bdc03b9b6def1f835540f43123ea55431289eb3a
Dmitry <>
2022-01-17 11:39:07 +0200

    Edit random movie messages to do not show bad quolity movies.

--- code/message_movie.py
+++ code/message_movie.py
@@ -17,22 +17,64 @@ def random_movie_value():
     # Read a csv file and create a random number.
     file = pd.read_csv('.data/data_v.3.0.csv')
     file_len = file[file.columns[0]].count() - 1
-    random_value = np.random.randint(0, file_len)
+
+    # Imlemented filters to show good quolity random movies.
+    while True:
+        random_value = np.random.randint(0, file_len)
+
+        # Remove from adult bad quolity movies.
+        if (file['ratingAgeLimits'][random_value] == 'r' or 'age18') and \
+           (file['ratingKinopoiskVoteCount'][random_value] >= 2000) and \
+           (file['ratingKinopoisk'][random_value] >= 5.5) and \
+           (file['year'][random_value] >= 2000):
+                break
+        else:
+            continue
+
+        # Remove from bad quolity movies.
+        if (file['ratingKinopoiskVoteCount'][random_value] >= 1000) and \
+           (file['ratingKinopoisk'][random_value] >= 5.0) and \
+           (file['year'][random_value] >= 2000):
+            break
+        else:
+            continue
+
+    # Rating kinopoisk vote count
+    if file['ratingKinopoiskVoteCount'][random_value] < 10000:
+        rating_kinopoisk_vote_count = '%.1f' %(file['ratingKinopoiskVoteCount'][random_value]/1000)
+    else:
+        rating_kinopoisk_vote_count = '%.0f' % (file['ratingKinopoiskVoteCount'][random_value] / 1000)
+
+    # Movie length
+    film_length = file['filmLength'][random_value]
 
     # Get link on a poster.
     image_link = file['posterUrl'][random_value]
 
-    # Test description.
-    test_description = 'Фантастический эпос про задыхающуюся Землю, космические ' \
-                       'полеты и парадоксы времени. «Оскар» за спецэффекты'
+    # Description.
+    description = str()
+    description_list = list(file['description'][random_value].split())
+
+    if len(description_list) > 30:
+        for word in description_list[0:30]:
+            description += word + ' '
+        description = f"\n\n\U0001F4D6{hcode(' Описание:')} {description}..."
+
+    elif len(description_list) == 0:
+        description = ''
+
+    else:
+        description = f"\n\n\U0001F4D6{hcode(' Описание:')} {file['description'][random_value]}"
+
 
     # Message view using aiogram markdown.
     text_value = f"{hbold(file['nameRu'][random_value])} " \
                  f"{hbold('(')}{hbold(file['year'][random_value])}{hbold(')')}\n\n" \
                  f"\U0001F31F{hcode(' Рейтинг:')}   {hbold(file['ratingKinopoisk'][random_value])}\n" \
-                 f"\U0001F440{hcode(' Оценки:')}     {hbold('523K')}\n" \
-                 f"\U0000231B{hcode(' Время:')}       {hbold('169 мин')}\n\n" \
-                 f"\U0001F4D6{hcode(' Описание:')} {test_description}"
+                 f"\U0001F440{hcode(' Оценки:')}     {hbold(rating_kinopoisk_vote_count)}{hbold('K')}\n" \
+                 f"\U0000231B{hcode(' Время:')}       {hbold(film_length, 'мин')}" \
+                 f"{description}"
+
 
     message_list = [image_link, text_value]
 
@@ -48,8 +90,6 @@ def random_movie_buttons():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
     return keyboard
-
-
 
 
 
